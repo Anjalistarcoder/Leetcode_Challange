66df30320e9cdcfe3a14599595fa9a13c6ef44a1
Anjalistarcoder <anjalistar077@gmail.com>
2026-07-17 23:54:39 +0530

    Time: 0 ms (100%), Space: 15.3 MB (16.62%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -9,44 +9,58 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
- class Solution {
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
-        if(root == NULL)
-            return ans;
-            queue<TreeNode*> q;
+        vector<vector<int>> result;
+        if (root == nullptr) {
+            return result;
+        }
+
+        queue<TreeNode*> q;
         q.push(root);
         bool leftToRight = true;
 
-        while(!q.empty()) {
-
+        while (!q.empty()) {
             int size = q.size();
+            deque<int> levelList;
 
-            vector<int> level(size);
-
-            for(int i = 0; i < size; i++) {
-
-                TreeNode* node = q.front();
+            for (int i = 0; i < size; ++i) {
+                TreeNode* currentNode = q.front();
                 q.pop();
 
-                int index;
+                // Add values to the correct side based on the traversal direction
+                if (leftToRight) {
+                    levelList.push_back(currentNode->val);
+                } else {
+                    levelList.push_front(currentNode->val);
+                }
 
-                if(leftToRight)
-                    index = i;
-                else
-                    index = size - 1 - i;
-
-                level[index] = node->val;
-
-                if(node->left)
-                    q.push(node->left);
-                    if(node->right)
-                    q.push(node->right);
+                if (currentNode->left != nullptr) {
+                    q.push(currentNode->left);
+                }
+                if (currentNode->right != nullptr) {
+                    q.push(currentNode->right);
+                }
             }
-            ans.push_back(level);
+
+            // Convert deque to vector and store it in result
+            result.push_back(vector<int>(levelList.begin(), levelList.end()));
+            // Toggle the direction for the next level
             leftToRight = !leftToRight;
         }
-        return ans;
+
+        return result;
     }
 };
